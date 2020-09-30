9ddfccbcb6deb5dbcf20805101cd3e5faee2d633
hide ogawa <mazarimono@gmail.com>
2020-09-30 11:15:09 +0900

    add ch07 dash pmc match showdata

--- ch07_dash_callback/dash_callback_pmc_match_showdata.py
+++ ch07_dash_callback/dash_callback_pmc_match_showdata.py
@@ -1,7 +1,7 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output, State, ALL
+from dash.dependencies import Input, Output, State, MATCH, ALLSMALLER
 import plotly.express as px
 
 # gapminderデータを読み込む
@@ -12,41 +12,42 @@ app = dash.Dash(__name__)
 # ➊ レイアウトの作成
 app.layout = html.Div(
     [
-        html.Button("PUSH ME", id="add_drop"),  # 新たなドロップダウンを追加するボタン（➋）
-        html.Div(id="show_drop", children=[]),  # ドロップダウンを追加するDiv（➌）
-        html.Div(id="my_text"),  # テキストを描画するDiv（➍）
+        html.Button("PUSH ME", id="add_drop"), 
+        html.Div(id="show_drop", children=[]),  # ドロップダウンと選択された値が追加されるUI
     ]
 )
 
-# ➎ コールバック1
+# ➋ コールバック1
 @app.callback(
     Output("show_drop", "children"),
     Input("add_drop", "n_clicks"),
     State("show_drop", "children"),
-    #prevent_initial_call=True,  # ➏
+    prevent_initial_call=True, 
 )
 def update_layout(n_clicks, children):
     new_layout = html.Div(
         [
             dcc.Dropdown(
-                id={"type": "my_dropdown", "index": n_clicks}, # ➐
+                id={"type": "my_dropdown", "index": n_clicks},
                 options=[{"label": c, "value": c} for c in gapminder.country.unique()],
                 value=gapminder.country.unique()[n_clicks-1],
+            ), # ➌ 文字列を表示するコンポーネント
+            html.Div(
+                id={"type": "text_show", "index": n_clicks}
             )
         ]
     )
-    children.append(new_layout) # ➑
+
+    children.append(new_layout)
     return children
 
 
-# ➒ コールバック2
+# ➍ コールバック2
 @app.callback(
-    Output("my_text", "children"),
-    Input({"type": "my_dropdown", "index": ALL}, "value"),  # ➓
-    prevent_initial_call=True,
+    Output({"type": "text_show", "index": MATCH}, "children"), # ➎
+    Input({"type": "my_dropdown", "index": MATCH}, "value"),  # ➏
 )
 def update_graph(selected_values):
-    # 全てのドロップダウンで選択された国のリストを文字列にする
     return str(selected_values)
 
 
